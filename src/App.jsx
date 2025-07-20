f23820e6b7ac9d14381f0815d04643e1066f146b
= <=>
2025-07-20 13:55:03 +0530

    minor bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import {
 } from "lucide-react";
 import Auth from "./pages/Auth.jsx"
 import {auth} from "./config/firebase.jsx"
+import {onAuthStateChanged} from "firebase/auth"
 
 import Announcement from "./pages/Announcement.jsx";
 
@@ -19,6 +20,24 @@ const App = () => {
   const [isLogin, setIsLogin] = useState(false);
     const [newAnnouncement, setNewAnnouncement] = useState('');
   const [isLoading, setIsLoading] = useState(false);
+
+
+    useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in.
+        setIsLogin(true);
+      } else {
+        // User is signed out.
+        setIsLogin(false);
+      }
+      setIsLoading(false); // <-- Stop loading once the check is complete
+    });
+
+        return () => unsubscribe();
+  }, []); // <-- The empty array means this effect runs only once on mount
+
+
 
 
   return (
